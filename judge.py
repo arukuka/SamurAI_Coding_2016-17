ef5e6204a7a60f0c025be697c49cc787bf4c5bfd
Shingo Ohtsuka <capriciouswalk@gmail.com>
2016-12-02 16:44:47 +0900

    塗った面積分報酬を大きくしてやる

--- judge.py
+++ judge.py
@@ -39,11 +39,11 @@ with open(result_path + 'painted.txt') as f:
 if result[0] > result[1]:
     print "left side win"
     with open(turn_path + "win.pickle", 'wb') as f:
-        pickle.dump([left[0], left[1], 10000, np.zeros((17, 15), dtype=np.int32), True], f)
+        pickle.dump([left[0], left[1], 100 * result[0], np.zeros((17, 15), dtype=np.int32), True], f)
 elif result[0] < result[1]:
     print "right side win"
     with open(turn_path + "win.pickle", 'wb') as f:
-        pickle.dump([right[0], right[1], 10000, np.zeros((17, 15), dtype=np.int32), True], f)
+        pickle.dump([right[0], right[1], 100 * result[1], np.zeros((17, 15), dtype=np.int32), True], f)
 else:
     print "draw"
     os.remove(turn_path + "win.pickle")
